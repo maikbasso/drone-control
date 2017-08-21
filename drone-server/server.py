7e8cb76c0dbd2d5250df13b2a2323896e798a9e2
Maik Basso <maik@maikbasso.com.br>
2017-08-21 16:29:45 -0300

    Adding submodule plant-line-detection

--- drone-server/server.py
+++ drone-server/server.py
@@ -20,7 +20,7 @@ drone = DroneControl("tcp:127.0.0.1:5760", 57600, "localhost", 7000, 12)
 #drone.registerClient("python ../drone-clients/test-shell/client-test-shell.py")
 #drone.registerClient("python ../drone-clients/test-square/client-test-square.py")
 #drone.registerClient("python ../drone-clients/test-virtual-joystick/client-test-virtual-joystick.py")
-#drone.registerClient("./../drone-clients/plant-line-detection/C++/build/main")
+#drone.registerClient("./../drone-clients/test-plant-line-detection/build/main")
 
 #start clients
 drone.startClients()
