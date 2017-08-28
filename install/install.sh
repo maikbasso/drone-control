701c2e8040b9f5fc47439c64539ab5cc24a03b7f
Maik Basso <maik@maikbasso.com.br>
2017-08-28 14:09:08 -0300

    Fix in install.sh

--- install/install.sh
+++ install/install.sh
@@ -3,10 +3,10 @@
 # @Author: Maik Basso <maik@maikbasso.com.br>
 
 # install linux dependencies
-sudo apt-get install -Y python-pip python-dev python-numpy python-opencv python-serial python-pyparsing python-wxgtk2.8
+sudo apt-get install -y python-pip python-dev python-numpy python-opencv python-serial python-pyparsing python-wxgtk2.8
 
 # install python dependencies
-sudo python install-python-dependencies.py
+sudo python install/install-python-dependencies.py
 
 # add current user in dialout group
 sudo adduser $USER dialout
