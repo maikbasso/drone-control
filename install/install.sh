9abbefbb6b024bc91f44d803a65ce8643f8ad346
Maik Basso <maik@maikbasso.com.br>
2017-08-28 13:56:56 -0300

    Fix install python dependencies

--- install/install.sh
+++ install/install.sh
@@ -6,7 +6,7 @@
 sudo apt-get install -Y python-pip python-dev python-numpy python-opencv python-serial python-pyparsing python-wxgtk2.8
 
 # install python dependencies
-sudo python install-python-depencencies.py
+sudo python install-python-dependencies.py
 
 # add current user in dialout group
 sudo adduser $USER dialout
