2ed7d519c8c5ef4b5b6120833e01763d3c771595
Maik Basso <maik@maikbasso.com.br>
2017-12-05 17:20:57 +0000

    Adding all commands in test-xbee-shell

--- drone-clients/test-xbee-shell/main.cpp
+++ drone-clients/test-xbee-shell/main.cpp
@@ -10,18 +10,26 @@ int main(){
 	
 	// Setup Xbee radio connection
 	WSNXbee *w = new WSNXbee("/dev/serial0", 57600);
-	usleep(5000000);
 	std::cout << "Connected with the Xbee\n";
+	
+	usleep(5000000);
 	
 	std::cout << "Select radio: ";
 	int xbeeNumber;
 	std::cin >> xbeeNumber;
+	
 	
 	int option = -1;
 	
 	while(option != -2){
 		std::cout << "0 - arm " << std::endl;
 		std::cout << "1 - setSpeed " << std::endl;
+		std::cout << "2 - takeOff " << std::endl;
+		std::cout << "3 - backToLand " << std::endl;
+		std::cout << "4 - returnToLaunch " << std::endl;
+		std::cout << "5 - rotateGimbal " << std::endl;
+		std::cout << "6 - setVelocity " << std::endl;
+		std::cout << "7 - setPosition " << std::endl;
 		std::cout << "Select an option > ";
 		std::cin >> option;
 		
@@ -41,6 +49,87 @@ int main(){
 				std::cin >> airSpeed;
 				
 				msg << std::fixed << "{\"command\":\"setSpeed\",\"args\":{\"airSpeed\":" << airSpeed << ",\"groundSpeed\":" << groundSpeed << "}}";
+				
+				std::cout << msg.str() << std::endl;
+				
+				w->send(xbeeNumber, msg.str());
+				break;
+			case 2:
+				std::cout << "z = ";
+				float z;
+				std::cin >> z;
+				
+				msg << std::fixed << "{\"command\":\"takeOff\",\"args\":{\"z\":" << z << "}}";
+				
+				std::cout << msg.str() << std::endl;
+				
+				w->send(xbeeNumber, msg.str());
+				break;
+			case 3:
+				w->send(xbeeNumber, "{\"command\":\"backToLand\",\"args\":{}}");
+				break;
+			case 4:
+				w->send(xbeeNumber, "{\"command\":\"returnToLaunch\",\"args\":{}}");
+				break;
+			case 5:
+				std::cout << "pitch = ";
+				float pitch;
+				std::cin >> pitch;
+				
+				std::cout << "roll = ";
+				float roll;
+				std::cin >> roll;
+				
+				std::cout << "yaw = ";
+				float yaw;
+				std::cin >> yaw;
+				
+				msg << std::fixed << "{\"command\":\"rotateGimbal\",\"args\":{\"pitch\":" << pitch << ",\"roll\":" << roll << ",\"yaw\":" << yaw << "}}";
+				
+				std::cout << msg.str() << std::endl;
+				
+				w->send(xbeeNumber, msg.str());
+				break;
+			case 6:
+				std::cout << "duration = ";
+				float duration;
+				std::cin >> duration;
+				
+				std::cout << "velocity_x = ";
+				float velocity_x;
+				std::cin >> velocity_x;
+				
+				std::cout << "velocity_y = ";
+				float velocity_y;
+				std::cin >> velocity_y;
+				
+				std::cout << "velocity_z = ";
+				float velocity_z;
+				std::cin >> velocity_z;
+				
+				msg << std::fixed << "{\"command\":\"setVelocity\",\"args\":{\"velocity_x\":" << velocity_x << ",\"velocity_y\":" << velocity_y << ",\"velocity_z\":" << velocity_z << ",\"duration\":" << duration << "}}";
+				
+				std::cout << msg.str() << std::endl;
+				
+				w->send(xbeeNumber, msg.str());
+				break;
+			case 7:
+				
+				std::cout << "px = ";
+				float px;
+				std::cin >> px;
+				
+				std::cout << "py = ";
+				float py;
+				std::cin >> py;
+				
+				std::cout << "pz = ";
+				float pz;
+				std::cin >> pz;
+				
+				msg << std::fixed << "{\"command\":\"setPosition\",\"args\":{\"x\":" << px << ",\"y\":" << py << ",\"z\":" << pz << "}}";
+				
+				std::cout << msg.str() << std::endl;
 				
 				w->send(xbeeNumber, msg.str());
 				break;
